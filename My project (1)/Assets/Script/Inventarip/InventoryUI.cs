c70c8b1815419b292086cf635323e827556d37c2
Yisoos <155551241+Yisoos@users.noreply.github.com>
2024-11-06 19:42:16 +0100

    added name of object

--- My project (1)/Assets/Script/Inventarip/InventoryUI.cs
+++ My project (1)/Assets/Script/Inventarip/InventoryUI.cs
@@ -10,26 +10,45 @@ public class InventoryUI : MonoBehaviour
     public Transform itemsParent;
     public GameObject itemSlotPrefab;
 
-    public void UpdateUI() 
+    public void UpdateUI()
     {
-    foreach (Transform child in itemsParent) 
+        foreach (Transform child in itemsParent)
         {
             Destroy(child.gameObject);
         }
+
         foreach (Item item in inventory.items)
         {
             GameObject newItemSlot = Instantiate(itemSlotPrefab, itemsParent);
+
+            // Set up the item for usage
             UseItem newItem = newItemSlot.GetComponent<UseItem>();
             newItem.item = item;
             newItem.inventory = inventory;
-            if(item.acumulable)
-            { 
-                newItemSlot.GetComponentInChildren<TMP_Text>().text = item.quantity.ToString();
-            }
-            else
+
+            // Retrieve TMP_Text components (assuming the first is for quantity, the second for item name)
+            TMP_Text[] textComponents = newItemSlot.GetComponentsInChildren<TMP_Text>();
+
+            if (textComponents.Length >= 2)
             {
-                newItemSlot.GetComponentInChildren<TMP_Text>().text = null;
+                TMP_Text quantityText = textComponents[0];  // First TMP_Text for quantity
+                TMP_Text nameText = textComponents[1];      // Second TMP_Text for item name
+
+                // Set quantity text based on whether the item is acumulable
+                if (item.acumulable)
+                {
+                    quantityText.text = item.quantity.ToString();
+                }
+                else
+                {
+                    quantityText.text = null;
+                }
+
+                // Set name text to the item's name
+                nameText.text = item.itemName;
             }
+
+            // Set the item icon
             newItemSlot.GetComponentInChildren<Image>().sprite = item.icon;
         }
     }
