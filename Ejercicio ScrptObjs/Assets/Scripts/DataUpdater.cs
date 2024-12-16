b38a3cc48e3360da99f518be98ff6e85f86d7cd1
Yisoos <jesus.isaias.martinez@estudiante.epsum.school>
2024-12-16 14:24:06 +0100

    Avance

---
+++ Ejercicio ScrptObjs/Assets/Scripts/DataUpdater.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataUpdater : MonoBehaviour
+{
+    public TextUpdater textUpdater;
+    // Start is called before the first frame update
+    // Update is called once per frame
+
+    private void Start()
+    {
+        if (textUpdater.nameInputField != null)
+        {
+            textUpdater.nameInputField.text = textUpdater.datosGlobales.nombreJugador;
+        }
+            CambiarNombreJugador();
+        CambiarDificultad(textUpdater.datosGlobales.dificultad);
+        CambiarPuntuación(0);
+    }
+    void Update()
+    {
+        if (textUpdater.nameInputField !=null)
+        {
+            if (textUpdater.nameInputField.text != textUpdater.datosGlobales.nombreJugador)
+            {
+                CambiarNombreJugador();
+            }
+        }
+    }
+
+    public void CambiarNombreJugador() 
+    {
+        if (textUpdater.nameInputField != null)
+        {
+            textUpdater.datosGlobales.nombreJugador = textUpdater.nameInputField.text;
+        }
+
+        if (textUpdater != null)
+        {
+            textUpdater.UpdateTextPlayerName();
+        }
+    }
+    public void CambiarDificultad(string nuevaDificultad) 
+    {
+        textUpdater.datosGlobales.dificultad = nuevaDificultad;
+        
+        if (textUpdater != null)
+        {
+            textUpdater.UpdateTextPlayerName();
+        }
+    }
+    public void CambiarPuntuación(int nuevaPuntuacion)
+    {
+        textUpdater.datosGlobales.puntuacion += nuevaPuntuacion;
+
+        if (textUpdater != null)
+        {
+            textUpdater.UpdateTextScore();
+        }
+    }
+
+}
--- Ejercicio ScrptObjs/Assets/Scripts/DatosGlobales.cs
+++ Ejercicio ScrptObjs/Assets/Scripts/DatosGlobales.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName ="DatosGlobales", menuName ="Datos/Datos Globales")]
 public class DatosGlobales : ScriptableObject
 {
